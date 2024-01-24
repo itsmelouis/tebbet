8509fe1cc7efd13425b275c15c69406f7de5aefc
LBERTAUD <lucas.bertaud@lenord.fr>
2024-01-24 15:44:22 +0100

    coming races

---
+++ Models/ComingRaces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tebbet.Models
+{
+    public class ComingRaces
+    {
+        public required string Image { get; set; }
+        public required string Title { get; set; }
+
+        public required string Date { get; set; }
+
+        public required string Adress { get; set; }
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,87 @@
-﻿namespace Tebbet.ViewModels;
+﻿using Avalonia.Media.Imaging;
+using ImageExample.Helpers;
+using ReactiveUI;
+using System;
+using System.ComponentModel;
 
-public class MainWindowViewModel : ViewModelBase
+
+namespace Tebbet.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Blabla => "Welcome to Avalonia !";
-#pragma warning restore CA1822 // Mark members as static
+    private string _HeaderComingRace;
+    private string _DateComingRace;
+    private string _AdressComingRace;
+    private Bitmap? _ImageComingRace;
+    public Bitmap? ImageComingRace
+    {
+        get => _ImageComingRace;
+        set
+        {
+            if (_ImageComingRace != value)
+            {
+                _ImageComingRace = value;
+                this.RaisePropertyChanged(nameof(ImageComingRace));
+            }
+        }
+    }
+
+    public string HeaderComingRace
+    {
+        get => _HeaderComingRace;
+        set
+        {
+            if ( _HeaderComingRace != value)
+            {
+                _HeaderComingRace = value;
+                this.RaisePropertyChanged(nameof(HeaderComingRace));
+            }
+        }
+    }
+
+    public string DateComingRace
+    {
+        get => _DateComingRace;
+        set
+        {
+            if ( _DateComingRace != value)
+            {
+                _DateComingRace = value;
+                this.RaisePropertyChanged(nameof(DateComingRace));
+            }
+        }
+    }
+
+    public string AdressComingRace
+    {
+        get => _AdressComingRace;
+        set
+        {
+            if (_AdressComingRace != value)
+            {
+                _AdressComingRace = value;
+                this.RaisePropertyChanged(nameof(AdressComingRace));
+            }
+        }
+    }
+    
+    public void setImageComingRace(string value)
+    {
+        ImageComingRace = ImageHelper.LoadFromResource(new Uri("avares://Tebbet/Assets/Images/Circuits/" + value));
+    }
+
+    public void setHeaderComingRace(string value)
+    {
+        HeaderComingRace = value;
+    }
+
+    public void setDateComingRace(string value)
+    {
+        DateComingRace = value;
+    }
+
+    public void setAdressComingRace(string value)
+    {
+        AdressComingRace = value;
+    }
 }
